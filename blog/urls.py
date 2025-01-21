2aedf239e5a822202b7c21900a7d3e255ed473f8
learnProject <leanProject@example.com>
2025-01-21 20:01:15 +0900

    feat: dd blog_detail.html

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, BlogDetailView
 
 urlpatterns = [
+    path("<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),
     path("", HomePageView.as_view(), name="home"),
 ]

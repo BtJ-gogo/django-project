d0d95b39d170ccf84fbe1a9bad6d1c7b3fcbd3ea
learnProject <leanProject@example.com>
2025-01-22 15:21:46 +0900

    feat: update urls for createview

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomePageView, BlogDetailView
+from .views import HomePageView, BlogDetailView, BlogCreateView
 
 urlpatterns = [
+    path("create/", BlogCreateView.as_view(), name="blog_create"),
     path("<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),
     path("", HomePageView.as_view(), name="home"),
 ]

74ba5aac8e99322a9f4217d45fb28fcf6bda02ea
learnProject <leanProject@example.com>
2025-01-22 15:41:20 +0900

    feat: add to model get_absolute_url

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from django.urls import reverse
+
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
@@ -11,3 +13,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse("blog_detail", kwargs={"pk": self.pk})
